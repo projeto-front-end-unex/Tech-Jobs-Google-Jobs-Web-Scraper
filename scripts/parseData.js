02383c968c428ec0ebc2dd927911dfcecea67cec
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2023-11-05 13:28:11 -0300

    feat(parseData ): adiciona os seguintes elementos durante o scraping: a logo da empresa, a descrição da vaga e os links para os sites onde as vagas estão cadastradas.

--- scripts/parseData.js
+++ scripts/parseData.js
@@ -7,14 +7,26 @@ function parseData(html) {
   const jobs = [];
   jobList.each(function(){
     const title = $(this).find('h2.KLsYvd').text();
-    const location = $(this).find('.sMzDkb').text();
-    const company = $(this).find('.nJlQNd').text();
-    const salary = $(this).find('.LL4CDc span').text();
+    const company = $(this).find('.nJlQNd.sMzDkb').text();
+    const location = $(this).find('.sMzDkb:nth-of-type(2)').text();
+    const salary = $(this).find('.I2Cbhb.bSuYSc .LL4CDc span').text();
+    const description = $(this).find('.YgLbBe.YRi0le .HBvzbc').text().trim().substring(0, 255) + "...";    
+    const companyLogo = $(this).find('.eZUcuf img').attr('src');
+    const links = [];
+    $(this).find('.B8oxKe.BQC79e.xXyUwe span a').each(function () {
+      const linkText = $(this).text();
+      const linkHref = $(this).attr('href');
+      links.push({ content: linkText, href: linkHref });
+    });
+    
     jobs.push({
       Title: title,
+      Salary: salary,
       Location: location,
       Company: company,
-      Salary: salary
+      CompanyLogo: companyLogo,
+      Description: description,
+      Links: links
     });
   });
 
