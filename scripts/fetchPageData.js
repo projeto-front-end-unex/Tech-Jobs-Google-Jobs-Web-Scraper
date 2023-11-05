53f9ada1e31128248c840d6e6df2125ed754127d
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2023-11-05 18:05:15 -0300

    feat(docker): adiciona Dockerfile para configuração do ambiente

--- scripts/fetchPageData.js
+++ scripts/fetchPageData.js
@@ -1,7 +1,20 @@
 const puppeteer = require('puppeteer');
+require("dotenv").config();
 
 async function fetchPageData(url) {
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36');
   await page.goto(url);
