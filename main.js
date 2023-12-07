94c74f4c32e0d60f5396a69d7aa8072c43e96906
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2023-12-07 13:20:36 -0300

    feat(main.js): Altera o array de jobUrls para incluir mais categorias de vagas

--- main.js
+++ main.js
@@ -5,28 +5,36 @@ const { setupAPI } = require('./api/jobs.js');
 
 const jobUrls = [
   {
+    label: 'desenvolvedor-front-end-jr',
+    url: 'https://www.google.com/search?q=Desenvolvedor+Front-end+Júnior&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=wlOig2jB39xFLSpxAAAAAA%3D%3D',
+  },
+  {
     label: 'desenvolvedor-back-end-jr',
     url: 'https://www.google.com/search?q=Desenvolvedor+Back-end+Júnior&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htilrad=-1.0&htichips=&htischips=&htiltype=1&htidocid=BJ1N8ByRp5B9IZOwAAAAAA%3D%3D',
   },
   {
-    label: 'desenvolvedor-front-end-jr-estagiario',
-    url: 'https://www.google.com/search?q=Desenvolvedor+Front-end+estagio&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=RdlDCsa4tq3htr43AAAAAA%3D%3D',
-  },
-  {
-    label: 'estagiario-de-desenvolvimento-web',
-    url: 'https://www.google.com/search?q=Estagiário+de+Desenvolvimento+Web&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=RdlDCsa4tq3htr43AAAAAA%3D%3D',
-  },
-  {
     label: 'desenvolvedor-mobile-junior',
-    url: 'https://www.google.com/search?q=Desenvolvedor+Mobile+Júnior&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htiltype=1&htidocid=cvKrgXdSct93gwBqAAAAAA%3D%3D',
+    url: 'https://www.google.com/search?q=Desenvolvedor+Mobile+Júnior&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htiltype=1&htidocid=b34cZiWrLPqMNXFaAAAAAA%3D%3D',
   },
   {
-    label: 'desenvolvedor-php-junior',
-    url: 'https://www.google.com/search?q=desenvolvedor+junior&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=gk4aQox_YQpbkOoEAAAAAA%3D%3D'
+    label: 'designer-ui-ux-jr',
+    url: 'https://www.google.com/search?q=Designer+UI/UX+Júnior&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=6LLPJ3U_zXMImjPHAAAAAA%3D%3D',
   },
   {
-    label: 'desenvolvedor-junior-presencial',
-    url: 'https://www.google.com/search?q=desenvolvedor+junior&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=gk4aQox_YQpbkOoEAAAAAA%3D%3D',
+    label: 'desenvolvedor-front-end-estagio',
+    url: 'https://www.google.com/search?q=Desenvolvedor+Front-end+estágio&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=Pch6YGNa1lkN6xvnAAAAAA%3D%3D',
+  },
+  {
+    label: 'desenvolvedor-mobile-estagio',
+    url: 'https://www.google.com/search?q=Desenvolvedor+mobile+estágio&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=WRUPKAkg7Bui7TS2AAAAAA%3D%3D'
+  },
+  {
+    label: 'desenvolvedor-back-end-estagio',
+    url: 'https://www.google.com/search?q=Desenvolvedor+Back-end+estágio&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htichips=&htischips=&htilrad=-1.0&htiltype=1&htidocid=p_XTYd76VXHy0lLcAAAAAA%3D%3D',
+  },
+  {
+    label: 'designer-ui-ux-estagio',
+    url: 'https://www.google.com/search?q=Designer+UI/UX+estágio&source=lmns&bih=955&biw=1270&client=opera&hs=75q&hl=pt-BR&sa=X&ibp=htl;jobs&ved=2ahUKEwjhx_vh3KuCAxVWIbkGHeblBTUQudcGKAF6BAgNEAk&sxsrf=AM9HkKlGITkTc7jIznwS_bQyuN89QfALXw:1699148323554#fpstate=tldetail&htivrt=jobs&htidocid=hgzIQ0ig1_1z5OHfAAAAAA%3D%3D',
   }
 ];
 
